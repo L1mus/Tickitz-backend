2206071681bad0f3d70eb4fd845d1481070960e9
M. Hanif Irfan <mhirfan173@gmail.com>
2026-06-08 21:43:49 +0700

    chore: adjust repository to get profile

--- internal/repository/user.repository.go
+++ internal/repository/user.repository.go
@@ -19,17 +19,21 @@ func NewUserRepository(db *pgxpool.Pool) *UserRepository {
 
 func (r *UserRepository) GetUserProfile(ctx context.Context, userID int) (*model.UserProfile, error) {
 	const q = `
-		SELECT
+		SELECT id,
 			COALESCE(first_name, '') AS first_name,
 			COALESCE(last_name, '') AS last_name,
 			email,
-			COALESCE(phone, '') AS phone
+			COALESCE(phone, '') AS phone,
+			COALESCE(photo, '') AS photo,
+			point,
+			created_at,
+			updated_at
 		FROM users
 		WHERE id = $1`
 
 	row := r.db.QueryRow(ctx, q, userID)
 	var up model.UserProfile
-	err := row.Scan(&up.FirstName, &up.LastName, &up.Email, &up.Phone)
+	err := row.Scan(&up.Id, &up.FirstName, &up.LastName, &up.Email, &up.Phone, &up.Photo, &up.Point, &up.Created_At, &up.Updated_At)
 	if err != nil {
 		return nil, err
 	}
