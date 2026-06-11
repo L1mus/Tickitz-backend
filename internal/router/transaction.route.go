c8866abb42f5867427f3fea4aa25b502e8f689f2
l1mus <ali.firefox@yahoo.co.id>
2026-06-11 20:36:17 +0700

    feat: add route for transaction

--- internal/router/transaction.route.go
+++ internal/router/transaction.route.go
@@ -1,11 +1,28 @@
 package router
 
 import (
+	"github.com/L1mus/Tickitz-backend/internal/controller"
+	"github.com/L1mus/Tickitz-backend/internal/middleware"
+	"github.com/L1mus/Tickitz-backend/internal/repository"
+	"github.com/L1mus/Tickitz-backend/internal/service"
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/redis/go-redis/v9"
 )
 
-func TransactionRouter(router *gin.Engine, db *pgxpool.Pool, rdb *redis.Client) {
-	// transactionRouter := router.Group("/transactions")
+func TransactionRouter(router *gin.RouterGroup, db *pgxpool.Pool, rdb *redis.Client) {
+	transactionRouter := router.Group("/transactions")
+	transactionRouter.Use(middleware.VerifyToken)
+	transactionRepo := repository.NewTransactionRepository()
+	orderRepo := repository.NewOrderRepository()
+	transactionService := service.NewTransactionService(transactionRepo, orderRepo, db)
+	transactionController := controller.NewTransactionController(transactionService)
+
+	transactionRouter.GET("/payment", transactionController.GetPaymentInformation)
+	transactionRouter.POST("/submit", transactionController.SubmitPayment)
+	transactionRouter.POST("/confirm", transactionController.ConfirmPayment)
+	transactionRouter.GET("/result", transactionController.GetResultTicket)
+	transactionRouter.GET("/qr", transactionController.GetQrCodeImage)
+	transactionRouter.POST("/checkout", transactionController.TryCheckOutDoku)
+	transactionRouter.POST("/doku-callback", transactionController.TryCallback)
 }
