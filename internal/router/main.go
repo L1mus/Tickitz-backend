c784648ae70e6366edbf363aa63b947cd9e16274
Akmal Rian <akmalrian@gmail.com>
2026-06-11 13:10:45 +0700

    feat : add admin movie route for main router

--- internal/router/main.go
+++ internal/router/main.go
@@ -28,6 +28,7 @@ func InitRouter(router *gin.Engine, db *pgxpool.Pool, rdb *redis.Client, mailer 
 	MovieRouter(routeApi, db, rdb)
 	AuthRouter(routeApi, db, rdb, mailer)
 	OrderRouter(routeApi, db, rdb)
+	AdminMovieRouter(routeApi, db, rdb)
 
 	//fallback
 	router.NoRoute(func(ctx *gin.Context) {
