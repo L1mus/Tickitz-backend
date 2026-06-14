626fc9db36b47a30bce0008e3b55ab1c3039a7a7
l1mus <ali.firefox@yahoo.co.id>
2026-06-15 03:48:05 +0700

    refactor: add cors allowed origin resource sharing

--- internal/middleware/cors.middleware.go
+++ internal/middleware/cors.middleware.go
@@ -8,29 +8,28 @@ import (
 )
 
 func CORSMiddleware(ctx *gin.Context) {
-    allowedOrigin := []string{
-        "http://localhost:5173", 
-        "http://localhost:3000", 
-        "http://localhost:9000", 
-        "http://localhost:6379", 
-        "http://localhost:200", 
-    }
+	allowedOrigin := []string{
+		"http://localhost:5173",
+		"http://localhost:6379",
+		"http://localhost:200",
+		"http://192.168.50.148:200",
+		"http://192.168.50.61:200",
+	}
 
-    currentOrigin := ctx.GetHeader("Origin")
+	currentOrigin := ctx.GetHeader("Origin")
 
-    if slices.Contains(allowedOrigin, currentOrigin) {
-        ctx.Header("Access-Control-Allow-Origin", currentOrigin)
-    }
-    ctx.Header("Access-Control-Allow-Credentials", "true")
+	if slices.Contains(allowedOrigin, currentOrigin) {
+		ctx.Header("Access-Control-Allow-Origin", currentOrigin)
+	}
+	ctx.Header("Access-Control-Allow-Credentials", "true")
 
+	ctx.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	ctx.Header("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
 
-    ctx.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-    ctx.Header("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
+	if ctx.Request.Method == http.MethodOptions {
+		ctx.AbortWithStatus(http.StatusNoContent)
+		return
+	}
 
-    if ctx.Request.Method == http.MethodOptions {
-        ctx.AbortWithStatus(http.StatusNoContent)
-        return
-    }
-
-    ctx.Next()
+	ctx.Next()
 }
